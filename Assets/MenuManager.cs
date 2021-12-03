cf74550ae894e6201e5d2e14a63fdaa97ec592f2
OthonielBaltazar <2018371011@uteq.edu.mx>
2021-12-03 13:50:27 -0600

    Beta

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	
+	public void BotonStart () {
+        SceneManager.LoadScene("HistoriaUno");
+	}
+
+    public void BotonQuit()
+    {
+        Debug.Log("Se cierra el juego");
+        Application.Quit();
+    }
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
